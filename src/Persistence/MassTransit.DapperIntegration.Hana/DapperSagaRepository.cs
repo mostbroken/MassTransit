33978558b4768c950ec54233c81878491ef24c7f
Meleshin, Evgeny <evgeny.meleshin@x5.ru>
2023-03-20 00:11:33 +0300

    Добавлена поддержка Dapper+SAP HAHA

---
+++ src/Persistence/MassTransit.DapperIntegration.Hana/DapperSagaRepository.cs
@@ -0,0 +1,26 @@
+﻿namespace MassTransit
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Reflection;
+    using Dapper;
+    using Dapper.Contrib.Extensions;
+    using DapperIntegration.Saga;
+    using Saga;
+
+
+    public static class DapperSagaRepository<TSaga>
+        where TSaga : class, ISaga
+    {
+        public static ISagaRepository<TSaga> Create(string connectionString, IsolationLevel isolationLevel = IsolationLevel.Serializable)
+        {
+            var consumeContextFactory = new SagaConsumeContextFactory<DatabaseContext<TSaga>, TSaga>();
+
+            var options = new DapperOptions<TSaga>(connectionString, isolationLevel);
+            var repositoryContextFactory = new DapperSagaRepositoryContextFactory<TSaga>(options, consumeContextFactory);
+
+            return new SagaRepository<TSaga>(repositoryContextFactory);
+        }
+    }
+}
